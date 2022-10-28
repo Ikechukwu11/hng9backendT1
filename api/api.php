c9239f8640ed24c4e6b2233b6d8087fd33c047b5
Ikechukwu.kev <kevinuzoma@gmail.com>
2022-10-28 18:39:03 +0100

    HNG9 Backend Task 1

---
+++ api/api.php
@@ -0,0 +1,22 @@
+<?php
+    if($_SERVER['REQUEST_METHOD']==="GET"){
+
+        $slackUsername="kekesmovic";
+        $backend=true; 
+        $age = 29;
+        $bio = "I am a fullstack developer and a gamer. I am here to make new techie friends, expand my horizon and gain experience on this internship program.";
+
+        $details = array(
+            "slackUsername"=> $slackUsername, 
+            "backend"=>$backend,
+            "age"=>$age,
+            "bio"=>$bio
+        );
+
+        // Use json_encode() function 
+        $json = json_encode($details); 
+          
+        // Display the output 
+        echo($json); 
+    } 
+?>
